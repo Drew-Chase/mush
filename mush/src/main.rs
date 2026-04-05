df48a9078ba3631803b2229d6d612d05b0be19ce
Drew (Home) <dcmanproductions@gmail.com>
2026-04-05 01:20:53 -0400

    Restore `ratatui` application loop and integrate with refactored `App` module under `widgets`.

--- mush/src/main.rs
+++ mush/src/main.rs
@@ -1,10 +1,9 @@
-mod app;
-mod command_input;
 mod config;
+mod widgets;
 
 use std::env::set_current_dir;
-use crate::app::App;
 use crate::config::Config;
+use crate::widgets::App;
 
 fn main() -> color_eyre::Result<()> {
     #[cfg(debug_assertions)]
@@ -12,10 +11,10 @@ fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     Config::load_or_default("./config.toml")?;
     Config::get().save()?;
-//    ratatui::run(|terminal| -> color_eyre::Result<()> {
-//        let mut instance = App::default();
-//        instance.run(terminal)?;
-//        Ok(())
-//    })?;
+    ratatui::run(|terminal| -> color_eyre::Result<()> {
+        let mut instance = App::default();
+        instance.run(terminal)?;
+        Ok(())
+    })?;
     Ok(())
 }
