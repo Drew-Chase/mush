bd1386795652125718c5e71e3eb51ba3369e499d
Drew (Home) <dcmanproductions@gmail.com>
2026-04-10 18:48:19 -0400

    Add persistent config directory detection via `dirs` crate
    Utilize the `dirs` crate to determine a persistent config path for `mush`, falling back to system-specific defaults if not explicitly set. Update `Config` loading/saving to use the new path, ensuring proper directory creation.

--- mush/src/main.rs
+++ mush/src/main.rs
@@ -3,15 +3,16 @@ mod db;
 mod shell;
 mod widgets;
 
-use std::env::set_current_dir;
 use crate::config::Config;
 use crate::widgets::App;
+use std::env::set_current_dir;
+use std::path::PathBuf;
+
 
 fn main() -> color_eyre::Result<()> {
-    #[cfg(debug_assertions)]
-    set_current_dir("target/")?;
     color_eyre::install()?;
-    Config::load_or_default("./config.toml")?;
+    std::fs::create_dir_all(get_appdata_path())?;
+    Config::load_or_default(get_appdata_path().join("config.toml"))?;
     Config::get().save()?;
     ratatui::run(|terminal| -> color_eyre::Result<()> {
         let mut instance = App::new()?;
@@ -19,4 +20,14 @@ fn main() -> color_eyre::Result<()> {
         Ok(())
     })?;
     Ok(())
+}
+
+pub fn get_appdata_path() -> PathBuf {
+    if let Ok(config_path) = std::env::var("MUSH_APPDATA") {
+        PathBuf::from(config_path)
+    } else if let Some(local_appdata) = dirs::config_local_dir() {
+        local_appdata.join("mush")
+    } else {
+        dirs::config_dir().unwrap().join("mush")
+    }
 }
